d11681260b11463e3d57eaaacdfd664dc9dbd828
Taofeek-haruna <taofeekharuna5@gmail.com>
2022-10-14 18:29:17 -0700

    first pro

---
+++ 0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <time.h>
+/* let make mr n */
+
+/*betty style for main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%di is negative\n", n);
+	}
+	return (0);
+}
