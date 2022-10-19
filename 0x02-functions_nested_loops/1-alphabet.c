39ae276c4824c42e3f1daf319f686816cd7b0ede
Taofeek-haruna <taofeekharuna5@gmail.com>
2022-10-18 20:08:47 -0700

    now

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,4 +1,15 @@
-#ifndef MAIN_H
-#define MAIN_H
 #include "main.h"
+
+/**
+* main - print the alphabelt in lowercase
+* it is coded in c
+* Return: 0
+*/
+
 void print_alphabet(void);
+{
+i = "I sometimes suffer from insomnia. And when I can't fall asleep,"
+y = "I play what I call the alphabet game"
+_putchar(i, y, lowercase);
+_putchar('\n');
+}
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+*main - check for lowercase characters
+*coded by c
+*Return:1 & 0
+*/
+int _islower(int c);
+{
+char ch = 'a';
+
+while (ch <= 'z')
+{
+printf("%c", ch);
+c++;
+}
+{
+if(if c is lowercase);
+return (1);
+
+else ();
+return (0);
+}
+}
