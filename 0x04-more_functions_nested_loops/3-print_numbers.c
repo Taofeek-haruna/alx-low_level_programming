1d72218b84ccfef5750f800c16c8ca63ff7ed660
Taofeek-haruna <taofeekharuna5@gmail.com>
2022-10-21 07:21:06 -0700

    0-5

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+* print_num - prints the num from 0 - 9
+* Retturn: void
+*/
+
+void print_numbers(void)
+{
+char c;
+
+for (c = '0'; c <= '9'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
