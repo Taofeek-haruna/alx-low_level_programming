b45d122099e746e00a83c6b95ba78b4c5a70eb08
Taofeek-haruna <taofeekharuna5@gmail.com>
2022-10-31 17:08:56 -0700

    yes

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+* _memcpy - functions copies @n bytes from memory area @src
+* to memory area @dest
+* @n: function copies
+* @src: bytes from memory area
+* @dest: to memory area
+* Return: a pointer to @dest
+*/
+char *_memcpy(char *dest, char *src, unsigned int n)
+
+{
+
+unsigned int y = 0;
+while (y < n)
+{
+dest[y] = src[y];
+y++;
+}
+return (dest);
+}
