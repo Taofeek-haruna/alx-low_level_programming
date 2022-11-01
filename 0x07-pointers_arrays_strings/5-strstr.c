121aeff19e0979a96c5a4d0b8b7b4228dfb58f3e
Taofeek-haruna <taofeekharuna5@gmail.com>
2022-10-31 21:16:09 -0700

    yes

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -5,49 +5,26 @@
  * @needle: the first occurrence of the substring
  * Return: a pointer beg of substring or @Null if it not foound.
  */
-
 char *_strstr(char *haystack, char *needle)
-
 {
-
 	unsigned int t = 0, o = 0;
 
-
-
 	while (haystack[t])
-
 	{
-
 		while (needle[o] && (haystack[t] == needle[0]))
-
 		{
-
 			if (haystack[t + o] == needle[o])
-
 				o++;
-
 			else
-
 				break;
-
 		}
-
 		if (needle[o])
-
 		{
-
 			t++;
-
 			o = 0;
-
 		}
-
 		else
-
 			return (haystack + t);
-
 	}
-
 	return (0);
-
 }
