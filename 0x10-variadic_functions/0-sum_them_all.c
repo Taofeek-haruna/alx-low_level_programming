ad0b0ece4bd62b6e14b4e6486dd861303f3b6504
Taofeek-haruna <taofeekharuna5@gmail.com>
2022-11-17 14:57:24 -0800

    it

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+* main - function that returns sum of all parameters
+* @const: constant declaration
+* @n: variable
+* Return: 0
+*/
+
+int sum_them_all(const unsigned int n, ...)
+{
+int sum;
+sum = sum_them_all(50, 60, 70);
+if (n == 0)
+{
+return (0);
+}
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,2 @@
+wow, we shall see it through
+its 0x10. C - Variadic functions again.
